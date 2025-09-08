c3547597d2af486086551efe24e4ed089deccd3b
HungNX247 <hungnx6666@gmail.com>
2025-09-08 16:42:52 +0700

    add hello2.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.err.println("Hello from Java in VS Code!");
+        System.out.println("Hello from Java in VS Code!");
     }
     
 }
