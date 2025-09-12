80ec72b1b008e83b0782b03e5f49d7bb7af7ea3f
HungNX247 <hungnx6666@gmail.com>
2025-09-12 20:52:18 +0700

    lesson01

---
+++ Lesson01/Lesson01/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 5;
+        int b = 10;
+        String result = "The sum of " + a + " and " + b + " is " + (a + b);
+        System.out.println(result);
+        System.out.println("Hello, World!");
+    }
+}
