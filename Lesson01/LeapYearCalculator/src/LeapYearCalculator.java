a7c63809484d2b653a71246cbec29d677dce1e9b
HungNX247 <hungnx6666@gmail.com>
2025-09-15 17:18:00 +0700

    add

---
+++ Lesson01/LeapYearCalculator/src/LeapYearCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a year: ");
+        year = scanner.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.printf("%d is a leap year", year);
+        } else {
+            System.out.printf("%d is NOT a leap year", year);
+        }
+        scanner.close();
+    }
+}
