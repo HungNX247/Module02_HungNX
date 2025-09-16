ece64ccab894847d963e969b2cbcbda9d8690bfe
HungNX247 <hungnx6666@gmail.com>
2025-09-16 09:25:02 +0700

    add

---
+++ Lesson01/Homework/ShowGreeting/src/ShowGreeting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ShowGreeting {
+    public static void main(String[] args) {
+        String name;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        name = scanner.nextLine();
+        System.out.println("Hello: " + name);
+        scanner.close();
+    }
+}
