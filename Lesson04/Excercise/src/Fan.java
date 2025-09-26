7b372b9be590399466c078abfbddb119db7fc644
HungNX247 <hungnx6666@gmail.com>
2025-09-26 12:19:33 +0700

    add

---
+++ Lesson04/Excercise/src/Fan.java
@@ -0,0 +1,70 @@
+public class Fan {
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+
+    private int speed = SLOW;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public Fan() {
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRaidus(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        if (on) {
+            return "Fan is on [speed = " + speed + ", color = " + color + ", radius = " + radius + "]";
+        } else {
+            return "Fan is off [color = " + color + ", radius = " + radius + "]";
+        }
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        fan1.setSpeed(Fan.FAST);
+        fan1.setColor("yellow");
+        fan1.setRaidus(10);
+        fan1.setOn(true);
+
+        Fan fan2 = new Fan();
+        fan2.setSpeed(Fan.MEDIUM);
+        fan2.setRaidus(5);
+        fan2.setColor("blue");
+        fan2.setOn(false);
+
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
+}
