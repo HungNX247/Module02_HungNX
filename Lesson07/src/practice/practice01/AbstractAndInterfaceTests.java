ebb7e79a72257e12c8a65c256535f2bac090f9ee
HungNX247 <hungnx6666@gmail.com>
2025-10-08 17:34:05 +0700

    add

--- Lesson07/src/practice/practice01/AbstractAndInterfaceTests.java
+++ Lesson07/src/practice/practice01/AbstractAndInterfaceTests.java
@@ -8,7 +8,7 @@ import practice.practice01.fruit.Apple;
 import practice.practice01.fruit.Fruit;
 import practice.practice01.fruit.Orange;
 
-public class bstractAndInterfaceTests {
+public class AbstractAndInterfaceTests {
     public static void main(String[] args) {
         Animal[] animals = new Animal[2];
         animals[0] = new Tiger();
