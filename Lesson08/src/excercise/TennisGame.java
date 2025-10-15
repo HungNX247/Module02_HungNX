d0962d9834c4fa5ada8dea8e6ffbe34e078a0470
HungNX247 <hungnx6666@gmail.com>
2025-10-15 15:13:34 +0700

    add

--- Lesson08/src/excercise/TennisGame.java
+++ Lesson08/src/excercise/TennisGame.java
@@ -3,60 +3,77 @@ package excercise;
 public class TennisGame {
     public static String getScore(String firstNamePlayer, String secondNamePlayer, int firstPlayerScore,
             int secondPlayerScore) {
-        String score = "";
-        int tempScore = 0;
+
         if (firstPlayerScore == secondPlayerScore) {
-            switch (firstPlayerScore) {
+            return getEqualScoreDescription(firstPlayerScore);
+        } else if (firstPlayerScore >= 4 || secondPlayerScore >= 4) {
+            return getAdvantageOrWinDescription(firstPlayerScore, secondPlayerScore, firstNamePlayer, secondNamePlayer);
+        } else {
+            return getNormalScoreDescription(firstPlayerScore, secondPlayerScore);
+        }
+    }
+
+    private static String getNormalScoreDescription(int firstPlayerScore, int secondPlayerScore) {
+        String scoreDescription = "";
+        int currentPlayerScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1)
+                currentPlayerScore = firstPlayerScore;
+            else {
+                scoreDescription += "-";
+                currentPlayerScore = secondPlayerScore;
+            }
+            switch (currentPlayerScore) {
                 case 0:
-                    score = "Love-All";
+                    scoreDescription += "Love";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    scoreDescription += "Fifteen";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    scoreDescription += "Thirty";
                     break;
                 case 3:
-                    score = "Forty-All";
+                    scoreDescription += "Forty";
                     break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
-        } else if (firstPlayerScore >= 4 || secondPlayerScore >= 4) {
-            int minusResult = firstPlayerScore - secondPlayerScore;
-            if (minusResult == 1)
-                score = "Advantage player1";
-            else if (minusResult == -1)
-                score = "Advantage player2";
-            else if (minusResult >= 2)
-                score = "Win for player1";
-            else
-                score = "Win for player2";
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1)
-                    tempScore = firstPlayerScore;
-                else {
-                    score += "-";
-                    tempScore = secondPlayerScore;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
             }
         }
+        return scoreDescription;
+    }
+
+    private static String getAdvantageOrWinDescription(int firstPlayerScore, int secondPlayerScore,
+            String firstNamePlayer,
+            String secondNamePlayer) {
+        String score;
+        int scoreDifference = firstPlayerScore - secondPlayerScore;
+        if (scoreDifference == 1)
+            score = "Advantage " + firstNamePlayer;
+        else if (scoreDifference == -1)
+            score = "Advantage " + secondNamePlayer;
+        else if (scoreDifference >= 2)
+            score = "Win for " + firstNamePlayer;
+        else
+            score = "Win for " + secondNamePlayer;
         return score;
+    }
+
+    private static String getEqualScoreDescription(int score) {
+        switch (score) {
+            case 0:
+                return "Love-All";
+
+            case 1:
+                return "Fifteen-All";
+
+            case 2:
+                return "Thirty-All";
+
+            case 3:
+                return "Forty-All";
+
+            default:
+                return "Deuce";
+
+        }
     }
 }
